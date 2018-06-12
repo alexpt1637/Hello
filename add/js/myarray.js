feb6f519248ce24aa58690c906ed392654174cf0
AlexPt <alexpt1637@yandex.ru>
2018-06-13 00:12:35 +1000

    установка значений для элементов массива

--- add/js/myarray.js
+++ add/js/myarray.js
@@ -8,8 +8,12 @@ var people = ["Tom", "Alice", "Sam"];	// можно добавить начальные данные
 
 console.log(people[0]); 	// Tom
 var person3 = people[2]; 	// Sam
-console.log(person3); 		// Sam
+//console.log('person3 = ' + person3); 		// Sam
 
-people[2] = "Bob";
+people[2] = "Gon";		// переопределение элемента массива
 var person3 = people[2];
-console.log('person3 = ' + person3);
+//console.log('person3 = ' + person3);
+//alert('person3 = ' + person3);
+// установка значений для элементов массива
+people[0] = "Bob";
+//console.log(people[0]); // Bob
