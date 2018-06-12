f929815ee4d5cb2b0d94fabf5936d21b472aa40f
AlexPt <alexpt1637@yandex.ru>
2018-06-12 23:59:50 +1000

    переопределение элемента массива

--- add/js/myarray.js
+++ add/js/myarray.js
@@ -2,6 +2,14 @@
 var myArray = new Array();
 
 // короткий способ инициализации массива:
-var myArray = [];						// создаем пустой массив
+var myArray = [];	// создаем пустой массив
 var people = ["Tom", "Alice", "Sam"];	// можно добавить начальные данные
-console.log(people);
+//console.log(people);
+
+console.log(people[0]); 	// Tom
+var person3 = people[2]; 	// Sam
+console.log(person3); 		// Sam
+
+people[2] = "Bob";
+var person3 = people[2];
+console.log('person3 = ' + person3);
